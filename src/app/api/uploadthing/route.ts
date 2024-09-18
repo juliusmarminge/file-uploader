986c7c69e4a02ea90e2983a6ef9c38ea4952e467
juliusmarminge <julius0216@outlook.com>
2024-09-19 00:43:14 +0200

    fix ut-v7

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -5,7 +5,7 @@ import { ourFileRouter } from "./core"
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-
+config: { logLevel: 'Debug'}
   // Apply an (optional) custom config:
   // config: { ... },
 })
--- src/hooks/use-upload-file.ts
+++ src/hooks/use-upload-file.ts
@@ -34,7 +34,7 @@ export function useUploadFile(
           setProgresses((prev) => {
             return {
               ...prev,
-              [file]: progress,
+              [file.name]: progress,
             }
           })
         },
